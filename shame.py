75093e0c89c3efa4d5c021eee3450a8a852d0949
sazwu <szwu00@gmail.com>
2024-02-17 02:45:15 -0800

    scraper

---
+++ shame.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import pip._vendor.requests as requests
+
+url = "https://www.nordstrom.com/s/llbean-quilted-quarter-zip-pullover/5715454?origin=coordinating-5715454-0-2-PDP_1-recbot-also_viewed_graph&recs_placement=PDP_1&recs_strategy=also_viewed_graph&recs_source=recbot&recs_page_type=product&recs_seed=5785205&color=LIGHT%20GRAY%20HEATHER"
+
+headers = {"User-Agent":"Mozilla/5.0"}
+html = requests.get(url, headers=headers).text
+
+def remove_tags(html):
+  soup = BeautifulSoup(html, "html.parser")
+  
+  for data in soup(['style', 'script', 'a', 'head', 'header', 'footer', 'form', 'iframe', 'link']):
+      # Remove tags
+      data.decompose()
+  
+  return ' '.join(soup.stripped_strings)
+
+print(remove_tags(html))
