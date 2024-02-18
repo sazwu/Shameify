58f60c9a7925589d6124ffa73e369256d0beaa08
Jenny Peng <jennypscale@gmail.com>
2024-02-17 23:39:12 -0800

    fix merge stuff fr

--- .web/utils/context.js
+++ .web/utils/context.js
@@ -1,15 +1,15 @@
 import { createContext, useContext, useMemo, useReducer, useState } from "react"
 import { applyDelta, Event, hydrateClientStorage, useEventLoop, refs } from "/utils/state.js"
 
-export const initialState = {"state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": ""}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "state.progress_example_state": {"show_progress": false}, "state.cond_simple_state": {"show": true}, "state.form_input_state": {"form_data": {}, "product_brand": "", "product_materials": "", "product_name": "", "roast_text": []}}
+export const initialState = {"state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": ""}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "state.cond_simple_state": {"show": true}, "state.progress_example_state": {"show_progress": false}, "state.form_input_state": {"form_data": {}, "product_brand": "", "product_materials": "", "product_name": "", "roast_text": []}}
 
 export const ColorModeContext = createContext(null);
 export const UploadFilesContext = createContext(null);
 export const DispatchContext = createContext(null);
 export const StateContexts = {
   state: createContext(null),
-  state__progress_example_state: createContext(null),
   state__cond_simple_state: createContext(null),
+  state__progress_example_state: createContext(null),
   state__form_input_state: createContext(null),
 }
 export const EventLoopContext = createContext(null);
@@ -54,29 +54,29 @@ export function EventLoopProvider({ children }) {
 
 export function StateProvider({ children }) {
   const [state, dispatch_state] = useReducer(applyDelta, initialState["state"])
-  const [state__progress_example_state, dispatch_state__progress_example_state] = useReducer(applyDelta, initialState["state.progress_example_state"])
   const [state__cond_simple_state, dispatch_state__cond_simple_state] = useReducer(applyDelta, initialState["state.cond_simple_state"])
+  const [state__progress_example_state, dispatch_state__progress_example_state] = useReducer(applyDelta, initialState["state.progress_example_state"])
   const [state__form_input_state, dispatch_state__form_input_state] = useReducer(applyDelta, initialState["state.form_input_state"])
   const dispatchers = useMemo(() => {
     return {
       "state": dispatch_state,
-      "state.progress_example_state": dispatch_state__progress_example_state,
       "state.cond_simple_state": dispatch_state__cond_simple_state,
+      "state.progress_example_state": dispatch_state__progress_example_state,
       "state.form_input_state": dispatch_state__form_input_state,
     }
   }, [])
 
   return (
     <StateContexts.state.Provider value={ state }>
-    <StateContexts.state__progress_example_state.Provider value={ state__progress_example_state }>
     <StateContexts.state__cond_simple_state.Provider value={ state__cond_simple_state }>
+    <StateContexts.state__progress_example_state.Provider value={ state__progress_example_state }>
     <StateContexts.state__form_input_state.Provider value={ state__form_input_state }>
       <DispatchContext.Provider value={dispatchers}>
         {children}
       </DispatchContext.Provider>
     </StateContexts.state__form_input_state.Provider>
-    </StateContexts.state__cond_simple_state.Provider>
     </StateContexts.state__progress_example_state.Provider>
+    </StateContexts.state__cond_simple_state.Provider>
     </StateContexts.state.Provider>
   )
 }
